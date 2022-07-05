e61c78259b5919fb3fe4ea4328377edb5dde28ed
JeongHyeSoo <nailonly@naver.com>
2022-07-05 14:24:52 +0900

    더하기 기능 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -7,14 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void 더하기(){
-        int rs = Calculator.add("10 + 20");
-        assertEquals(30,rs);
+        int rs = Calculator.add("10+20");
+        assertEquals(30, rs);
 
     }
     @Test
     public void 더하기_2(){
-        int rs = Calculator.add("20 + 20");
-        assertEquals(40,rs);
+        int rs = Calculator.add("20+20");
+        assertEquals(40, rs);
+    }
 
+    @Test
+    public void 더하기_3(){
+        int rs = Calculator.add("10+10");
+        assertEquals(20, rs);
     }
 }
